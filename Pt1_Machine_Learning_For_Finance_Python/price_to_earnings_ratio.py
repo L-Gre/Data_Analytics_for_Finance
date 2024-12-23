ef01793923485d32ebb085515ca4c4f456500e0a
M1sterDonut <lhg@tuta.io>
2024-12-23 21:49:39 +0100

    First Commit

---
+++ Pt1_Machine_Learning_For_Finance_Python/price_to_earnings_ratio.py
@@ -0,0 +1,5 @@
+## calculating price to earnings (P/E) ratio
+
+x, y = 220, 10
+
+print( 'Price = ${}; EPS = ${}; P/E = {}'.format(x,y,x/y) )
---
+++ Pt1_Machine_Learning_For_Finance_Python/rate_of_return.py
@@ -0,0 +1,9 @@
+## calculating rate of return (RoR)
+
+end_value = 30000
+start_value = 10000
+
+ror = ( ( end_value - start_value ) / start_value ) * 100
+
+ror = ( ( 30000 - 10000 ) / 10000 ) * 100
+ror
