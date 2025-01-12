4c6a4ab88b7d4833fa674d5501e5d264c2d32ea9
M1sterDonut <lhg@tuta.io>
2025-01-12 23:48:09 +0000

    Add step-sorted files

--- Pt2_Applied_Machine_Learning_Algorithms/Mod6_Investor_Classifier_II/libraries.py
+++ Pt2_Applied_Machine_Learning_Algorithms/Mod6_Investor_Classifier_II/libraries.py
@@ -6,6 +6,30 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import sklearn
 
+from sklearn.pipeline import make_pipeline # to set up the steps you follow with each model
+from sklearn.preprocessing import StandardScaler # to undo the influence of magnitude of large values
+
+from sklearn.linear_model import LogisticRegression # L1 and L2
+from sklearn.ensemble import RandomForestClassifier # rf
+from sklearn.ensemble import GradientBoostingClassifier # gb
+
+## functions
+
+import pickle
+
+def save_to_pickle(obj, filename):
+    with open(filename, "wb") as f:
+        pickle.dump(obj, f)
+
+def load_from_pickle(filename):
+    with open(filename, "rb") as f:
+        return pickle.load(f)
+
+# # Example usage
+# my_dict = {"key1": "value1"}
+# save_to_pickle(my_dict, "my_dict.pkl")
+# loaded_dict = load_from_pickle("my_dict.pkl")
+
 
 ## file path vars
 
